00bd70a3126b8fa9fa1b959837f251cc717bbc4b
Hubert Romaniak <hubert.romaniak@gmail.com>
2024-11-27 19:52:58 +0100

    PwJJ: LAB2

---
+++ 5 semestr/Programowanie w języku Java/PwJJ_LAB2/src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * Application entry point.
+ */
+public static void main() {
+	System.out.println(new Color((byte) 0, (byte) 1, (byte) 2));
+	System.out.println(new Color((byte) 3, (byte) 4, (byte) 5, (byte) 6));
+}

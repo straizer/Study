c14901efe749bff14d80ca39ff7543fd9937c3d2
Hubert Romaniak <hubert.romaniak@gmail.com>
2024-12-12 17:14:43 +0100

    PwJJ: make use of lombok`s @ExtensionMethod

---
+++ 5 semestr/Programowanie w języku Java/src/main/java/semester5/pwjj/utils/ArrayUtils.java
@@ -0,0 +1,22 @@
+package semester5.pwjj.utils;
+
+import lombok.experimental.ExtensionMethod;
+import lombok.experimental.UtilityClass;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/** Class containing native Java array utils. */
+@UtilityClass
+@ExtensionMethod(Arrays.class)
+public class ArrayUtils {
+
+	/**
+	 * Creates {@link Stream} from native Java array and performs {@link Stream#skip(long)} operation on it.
+	 * @see Stream#skip(long)
+	 */
+	public <T> Stream<T> skip(final @NonNull T @NonNull [] array, final long n) {
+		return array.stream().skip(n);
+	}
+}

af55e7fb1c2a981dcad09b3693c2a0947d5279cf
Hubert Romaniak <hubert.romaniak@gmail.com>
2025-01-17 03:12:12 +0100

    PwJJ: add utility class EnvProperties responsible for loading properties from `.env` file located at module root

--- 5 semestr/Programowanie w języku Java/src/test/java/semester5/pwjj/utils/UtilsTestsBase.java
+++ 5 semestr/Programowanie w języku Java/src/test/java/semester5/pwjj/utils/UtilsTestsBase.java
@@ -1,18 +1,12 @@
 package semester5.pwjj.utils;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.jupiter.api.BeforeAll;
 import semester5.pwjj.TestsBase;
-import semester5.pwjj.utils.i18n.I18nProperty;
 
 
 /** Abstract base class for {@code utils} test setups and utilities. */
 @SuppressWarnings({"ClassNamePrefixedWithPackageName", "unused"})
 public abstract class UtilsTestsBase extends TestsBase {
-
-	protected static final @NonNull I18nProperty NO_FILE_FOUND = Messages.Error.NO_FILE_FOUND;
-	protected static final @NonNull I18nProperty NO_READ_ACCESS = Messages.Error.NO_READ_ACCESS;
-	protected static final @NonNull I18nProperty READING_FAILED = Messages.Error.READING_FAILED;
 
 	/** Method executed once before all tests in each extending class. */
 	@BeforeAll

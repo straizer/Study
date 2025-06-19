02d922c321d38e88690588c31e495b70b0a6549f
Hubert Romaniak <hubert.romaniak@gmail.com>
2025-06-20 00:29:31 +0200

    WDPAI: create routers and controllers

--- 6 semestr/Wstęp do projektowania aplikacji internetowych/index.php
+++ 6 semestr/Wstęp do projektowania aplikacji internetowych/index.php
@@ -1,3 +1,18 @@
 <?php
+declare(strict_types=1);
 
-require 'public/views/login.html';
+require_once __DIR__ . '/autoload.php';
+
+use App\Router;
+
+$router = new Router();
+
+// Define routes
+$router->get('/', function() {
+    echo 'Home Page';
+});
+
+$router->get('/login', [App\Controllers\Auth\LoginController::class, 'index']);
+
+// Run the router
+$router->run();

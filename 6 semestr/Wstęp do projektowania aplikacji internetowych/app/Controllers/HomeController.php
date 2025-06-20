2d43cb8395df336a2e33f9fdb6ff9c9b13456aaa
Hubert Romaniak <hubert.romaniak@gmail.com>
2025-06-20 06:40:27 +0200

    WDPAI: move cards to models

--- 6 semestr/Wstęp do projektowania aplikacji internetowych/app/Controllers/HomeController.php
+++ 6 semestr/Wstęp do projektowania aplikacji internetowych/app/Controllers/HomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Models\Auth;
+use App\Models\ConnectedApp;
+use App\Models\HealthMetric;
 
 class HomeController extends Controller
 {
@@ -11,6 +13,13 @@ class HomeController extends Controller
 	{
 		Auth::requireAuth();
 		$user = Auth::user();
-		$this->render('Home', 'HomeView', ['user' => $user]);
+		$health_metrics = HealthMetric::getAll();
+		$connected_apps = ConnectedApp::getAll();
+
+		$this->render('Home', 'HomeView', [
+			'user' => $user,
+			'health_metrics' => $health_metrics,
+			'connected_apps' => $connected_apps,
+		]);
 	}
 }
--- 6 semestr/Wstęp do projektowania aplikacji internetowych/app/Views/HomeView.php
+++ 6 semestr/Wstęp do projektowania aplikacji internetowych/app/Views/HomeView.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 
 $user = $user ?? null;
+$health_metrics = $health_metrics ?? [];
+$connected_apps = $connected_apps ?? [];
 ?>
 
 <div class="home-content">
@@ -9,45 +11,26 @@ $user = $user ?? null;
 	<p class="health-overview">Here is your health overview for today</p>
 
 	<div class="health-cards">
-		<div class="health-card">
-			<p>Steps Today</p>
-			<h3>8,439</h3>
-			<i class="fa-solid fa-shoe-prints fit-icon" style="color: #0000ff;"></i>
-		</div>
-		<div class="health-card">
-			<p>Hearth Rate</p>
-			<h3>72 BPM</h3>
-			<i class="fa-solid fa-heart-pulse fit-icon" style="color: #ff0000;"></i>
-		</div>
-		<div class="health-card">
-			<p>Sleep</p>
-			<h3>7h 23 min</h3>
-			<i class="fa-solid fa-moon fit-icon" style="color: #800080;"></i>
-		</div>
-		<div class="health-card">
-			<p>Calories Burnt</p>
-			<h3>1,842</h3>
-			<i class="fa-solid fa-fire fit-icon" style="color: #ffa500;"></i>
-		</div>
+		<?php foreach ($health_metrics as $metric): ?>
+			<div class="health-card">
+				<p><?php echo htmlspecialchars($metric->getName()); ?></p>
+				<h3><?php echo htmlspecialchars($metric->getValue()); ?></h3>
+				<i class="<?php echo htmlspecialchars($metric->getIcon()); ?> fit-icon"
+				   style="color: <?php echo htmlspecialchars($metric->getIconColor()); ?>;"></i>
+			</div>
+		<?php endforeach; ?>
 	</div>
 	<div class="connect-cards">
 		<h3>Connected Apps</h3>
 		<div class="connect-cards-container">
-			<div class="connect-card">
-				<p>Fitbit</p>
-				<h3>Connected</h3>
-				<i class="fa-solid fa-ring fit-icon" style="color: #00b894;"></i>
-			</div>
-			<div class="connect-card">
-				<p>Apple Health</p>
-				<h3>Connected</h3>
-				<i class="fa-brands fa-apple fit-icon" style="color: #636e72;"></i>
-			</div>
-			<div class="connect-card">
-				<p>Google Fit</p>
-				<h3>Connected</h3>
-				<i class="fa-brands fa-google fit-icon" style="color: #4285f4;"></i>
-			</div>
+			<?php foreach ($connected_apps as $app): ?>
+				<div class="connect-card">
+					<p><?php echo htmlspecialchars($app->getName()); ?></p>
+					<h3><?php echo htmlspecialchars($app->getStatus()); ?></h3>
+					<i class="<?php echo htmlspecialchars($app->getIcon()); ?> fit-icon"
+					   style="color: <?php echo htmlspecialchars($app->getIconColor()); ?>;"></i>
+				</div>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </div>

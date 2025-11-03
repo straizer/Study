a8cd5ca16ed115dc4bd6cfea7cc122bbce5dbcc6
Hubert Romaniak <hubert.romaniak@gmail.com>
2025-11-03 07:38:24 +0100

    PUS: pass mutagen project to mount

--- 7 semestr/Programowanie usług sieciowych/PUS_LAB1/start.ps1
+++ 7 semestr/Programowanie usług sieciowych/PUS_LAB1/start.ps1
@@ -1,4 +1,15 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$Project
+)
+
 $ErrorActionPreference = "Stop"
+
+if (-not (Test-Path $Project))
+{
+    Write-Error "Directory '$Project' does not exist."
+    exit 1
+}
 
 # assume ensure-mutagen.ps1 exists in same directory
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -18,10 +29,14 @@ if (-not $mutagenExe)
 
 Write-Host "Using mutagen at: $mutagenExe"
 
+# clean previous session
+& $mutagenExe project terminate *> $null
+
 # 2. run docker compose
 Write-Host "Running: compose up --build --force-recreate --no-log-prefix --remove-orphans --quiet-pull -V --wait"
 docker compose up --build --force-recreate --no-log-prefix --remove-orphans --quiet-pull -V --wait
 
 # 3. start mutagen project
-Write-Host "Running: mutagen project start"
+(Get-Content mutagen.tpl.yml) -replace '{{ALPHA}}', $Project | Set-Content mutagen.yml
+Write-Host "Running: mutagen project start (alpha=$Project)"
 & $mutagenExe project start

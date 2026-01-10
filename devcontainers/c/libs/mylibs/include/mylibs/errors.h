03d890c270325039793bef2b42517ebdf88ad91c
Hubert Romaniak <hubert.romaniak@gmail.com>
2026-01-10 18:08:01 +0100

    devcontainer-c: expose error buffers

--- devcontainers/c/libs/mylibs/include/mylibs/errors.h
+++ devcontainers/c/libs/mylibs/include/mylibs/errors.h
@@ -1,5 +1,11 @@
 #pragma once
 
+enum {
+    ERROR_BUFFER_SIZE = 256,
+};
+
+char* getErrorBuffer(void);
+
 const char* prefixErrno(const char* prefix);
 const char* prefixError(const char* prefix, const char* message);
 const char* errorDuring(const char* primary_prefix, const char* primary_message, const char* secondary);
--- devcontainers/c/libs/mylibs/src/errors.c
+++ devcontainers/c/libs/mylibs/src/errors.c
@@ -10,12 +10,11 @@
 
 enum {
     ERROR_BUFFERS_SLOTS = 8,
-    ERROR_BUFFERS_SIZE = 256,
 };
 
 // cppcheck-suppress misra-c2012-1.4
 static _Thread_local char error_buffers  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
-    [ERROR_BUFFERS_SLOTS][ERROR_BUFFERS_SIZE];
+    [ERROR_BUFFERS_SLOTS][ERROR_BUFFER_SIZE];
 // cppcheck-suppress misra-c2012-1.4
 static _Thread_local size_t error_buffer_idx;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
@@ -24,10 +23,12 @@ static const char* formatError(const char* first, const char* prefix_second, con
 
 /* ------------------------------------------ Public function definitions ------------------------------------------ */
 
+char* getErrorBuffer(void) { return error_buffers[error_buffer_idx++ % ERROR_BUFFERS_SLOTS]; }
+
 const char* prefixErrno(const char* const prefix) {
     const int saved_errno = errno;
 
-    char tmp[ERROR_BUFFERS_SIZE];
+    char tmp[ERROR_BUFFER_SIZE];
     const char* const message = strerror_r(saved_errno, tmp, sizeof(tmp));
     const char* const out = prefixError(prefix, message);
 
@@ -48,15 +49,14 @@ const char* errorDuring(const char* const primary_prefix, const char* const prim
 
 static const char* formatError(const char* const first, const char* const prefix_second, const char* const second,
                                const char* const suffix_second) {
-    char* const out = error_buffers[error_buffer_idx % ERROR_BUFFERS_SLOTS];
-    error_buffer_idx++;
+    char* const out = getErrorBuffer();
 
     const bool first_valid = stringIsValid(first);
     const bool prefix_second_valid = stringIsValid(prefix_second);
     const bool second_valid = stringIsValid(second);
     const bool suffix_second_valid = stringIsValid(suffix_second);
 
-    (void)snprintf(out, ERROR_BUFFERS_SIZE, "%s%s%s%s", first_valid ? first : "",
+    (void)snprintf(out, ERROR_BUFFER_SIZE, "%s%s%s%s", first_valid ? first : "",
                    first_valid && second_valid && prefix_second_valid ? prefix_second : "", second_valid ? second : "",
                    first_valid && second_valid && suffix_second_valid ? suffix_second : "");
 
